c80cb869e46b8d08f2f9b6775a6a7a04750cdcf9
Thom Shutt <thomshutt@users.noreply.github.com>
2022-04-23 18:26:37 +0200

    Handle error

--- netlify/functions/hello/hello.go
+++ netlify/functions/hello/hello.go
@@ -23,6 +23,9 @@ func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResp
 	}
 
 	file, _, err := httpReq.FormFile("file")
+	if err != nil {
+		return nil, err
+	}
 	defer file.Close()
 
 	var buf bytes.Buffer
@@ -31,11 +34,11 @@ func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResp
 	responseBody += fmt.Sprintf("Buffer Length: %d", buf.Len())
 
 	return &events.APIGatewayProxyResponse{
-		StatusCode:        200,
-		Headers:           map[string]string{"Content-Type": "text/plain"},
-		MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
-		Body:              responseBody,
-		IsBase64Encoded:   false,
+		StatusCode: 200,
+		Headers:    map[string]string{"Content-Type": "text/plain"},
+		//MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
+		Body:            responseBody,
+		IsBase64Encoded: false,
 	}, nil
 }
 
