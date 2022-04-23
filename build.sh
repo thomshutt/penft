25332af3cd89b3a775c31e1d2c86dde66302bed7
Thom Shutt <thomshutt@users.noreply.github.com>
2022-04-23 13:06:08 +0200

    Install functions

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 go version
 go get github.com/dsoprea/go-exif/v3@v3.0.0-20210625224831-a6301f85c82b
 go mod download github.com/stretchr/testify
-go build ./...
-go install .
 GOBIN=$(pwd)/netlify/functions go install ./...
