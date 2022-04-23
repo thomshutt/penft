0c3a3902e97f3eb4816716adc0deadd88759bb41
Thom Shutt <thomshutt@users.noreply.github.com>
2022-04-23 13:16:29 +0200

    Start building function

--- netlify/functions/hello/hello.go
+++ netlify/functions/hello/hello.go
@@ -3,15 +3,26 @@ package main
 import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"io/ioutil"
 	"net/http"
 )
 
+const TATUM_API_KEY = "3e69a90d-1bd7-4994-ae9f-0aefe391b452"
+const imageFile = "public/images/image.jpg"
+
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	_, err := ioutil.ReadFile(imageFile)
+	if err != nil {
+		return nil, err
+	}
+
+	responseBody := "Hello, World!"
+
 	return &events.APIGatewayProxyResponse{
 		StatusCode:        200,
 		Headers:           map[string]string{"Content-Type": "text/plain"},
 		MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
-		Body:              "Hello, World!",
+		Body:              responseBody,
 		IsBase64Encoded:   false,
 	}, nil
 }
