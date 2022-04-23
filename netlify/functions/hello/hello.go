016de9f9a4e051e697526cbd2562bf8c5c54199d
Thom Shutt <thomshutt@users.noreply.github.com>
2022-04-23 17:59:58 +0200

    ********

--- netlify/functions/hello/hello.go
+++ netlify/functions/hello/hello.go
@@ -1,41 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"io/ioutil"
 	"net/http"
+	"os"
 )
 
-const TATUM_API_KEY = "3e69a90d-1bd7-4994-ae9f-0aefe391b452"
-const imageFile = "public/images/image.jpg"
+var TATUM_API_KEY = os.Getenv("TATUM_API_KEY")
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	files, err := ioutil.ReadDir(".")
-	if err != nil {
-		return nil, err
-	}
-
-	for _, file := range files {
-		fmt.Println("File in current dir: ", file.Name(), file.IsDir())
-	}
-
-	files, err = ioutil.ReadDir("..")
-	if err != nil {
-		return nil, err
-	}
-
-	for _, file := range files {
-		fmt.Println("File in parent dir: ", file.Name(), file.IsDir())
-	}
-
-	_, err = ioutil.ReadFile(imageFile)
-	if err != nil {
-		return nil, err
-	}
-
-	responseBody := "Hello, World!"
+	responseBody := "Hello, World! " + TATUM_API_KEY
 
 	return &events.APIGatewayProxyResponse{
 		StatusCode:        200,
