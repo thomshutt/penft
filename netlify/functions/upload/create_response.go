780dc111758a30baa9995a5c012a32c9eb692508
Thom Shutt <thomshutt@users.noreply.github.com>
2022-04-24 00:08:07 +0200

    Fix relative links

--- netlify/functions/upload/create_response.go
+++ netlify/functions/upload/create_response.go
@@ -15,14 +15,14 @@ func createResponse(ipfsHash string) string {
 
     <!-- CSS
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-    <link rel="stylesheet" href="css/pico.min.css">
+    <link rel="stylesheet" href="/css/pico.min.css">
 
 </head>
 <body>
 
 <header class="container" style="padding-bottom: 0px;">
     <hgroup style="text-align: center;">
-        <img src="images/banner.png" style="max-width:75%;"/>
+        <img src="/images/banner.png" style="max-width:75%;"/>
     </hgroup>
     <hgroup style="text-align:center;">
         <h2>partially-encryptedNFT: Proof-of-concept of partial encryption of image NFTs, to display and prove ownership
